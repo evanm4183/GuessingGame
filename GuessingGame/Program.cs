17c8d0e3307ede58799ec78a61e96147451f0bd0
Evan Mitchell <evanm4183@gmail.com>
2022-08-22 14:02:03 -0500

    phase 7 complete

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -10,7 +10,8 @@ namespace GuessingGame
             int secretNumber = random.Next(1, 100);
 
             Console.WriteLine("Guess the secret number!");
-            MultipleChances(4, secretNumber);
+            int numberOfChances = GetNumberOfChances();
+            MultipleChances(numberOfChances, secretNumber);
         }
 
         static void MultipleChances(int numberOfChances, int secretNumber)
@@ -40,6 +41,29 @@ namespace GuessingGame
                 {
                     Console.Write($"You used up all {numberOfChances} of your guesses. The secret number was {secretNumber}");
                 }
+            }
+        }
+
+        static int GetNumberOfChances()
+        {
+            Console.WriteLine("Choose your difficulty (Easy-1, Medium-2, Hard-3): ");
+            int difficulty = int.Parse(Console.ReadLine());
+
+            if (difficulty == 1)
+            {
+                return 8;
+            }
+            else if (difficulty == 2)
+            {
+                return 6;
+            }
+            else if (difficulty == 3)
+            {
+                return 4;
+            }
+            else {
+                Console.WriteLine("Not a valid input");
+                return 0;
             }
         }
 
